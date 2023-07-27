8dc17e2c68e1eefc64016903d5aca7336a7c5b91
stephen-cusi <magiskcurry@qq.com>
2023-07-27 13:18:07 +0800

    big update(it's true)

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  document.body.addEventListener("click", function() {
+    
+    var audio = new Audio("https://s27.aconvert.com/convert/p3r68-cdx67/eoano-15ehy.mp3");
+    audio.play();
+    
+  });
+
+});
