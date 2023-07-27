64b519f5003a69ae8f015a5b27b00d58aa08fd73
stephen-cusi <magiskcurry@qq.com>
2023-07-27 15:03:39 +0800

    Partial updates

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {
+// 原有点击播放音频代码
 
-  document.body.addEventListener("click", function() {
+document.addEventListener('DOMContentLoaded', () => {
+  let audio = new Audio('assets/granny_menu.mp3');
+  audio.play();
+});
 
-    var audio = new Audio("assets/granny_menu.mp3"); 
-    audio.play();
 
-  });
+// 添加底部按钮点击处理
+const btn = document.getElementById('grave-btn');
+
+btn.addEventListener('click', () => {
+
+  if(confirm('是否去看他的墓?')){
+    window.location.href = 'https://b23.tv/3Oks25P';
+  }
 
 });
