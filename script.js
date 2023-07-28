321c0f2948cb544f58fdf024da46e001efce92a7
stephen-cusi <magiskcurry@qq.com>
2023-07-28 11:00:26 +0800

    NetEase Cloud

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-// 原有点击播放音频代码
+// 使用网络音频链接 
+const audio = new Audio('https://m701.music.126.net/20230728111816/fddb07312ae64260d072b4422a908085/jdyyaac/5153/050f/5158/f9185c39882efd4ac54cf2c1824f6a2a.m4a');
 
 document.addEventListener('DOMContentLoaded', () => {
-  let audio = new Audio('assets/granny_menu.mp3');
-  audio.play();
-});
 
+  audio.play();
+
+});
 
 // 添加底部按钮点击处理
 const btn = document.getElementById('grave-btn');
